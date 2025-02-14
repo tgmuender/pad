3377727b4d853af6454661c72fb5e60244e07338
Tobias Gmünder <tgmuender@gmail.com>
2025-02-14 21:22:45 +0100

    Rename cli to 'petctl' and make 'help' the default command if none specified

--- cmd/cmd/root.go
+++ cmd/cmd/root.go
@@ -2,17 +2,19 @@ package cmd
 
 import "github.com/spf13/cobra"
 
+// NewRootCmd returns the root command for the application
 func NewRootCmd() *cobra.Command {
 	cmd := cobra.Command{
-		Use: "padctl",
+		Use: "petctl",
 		Run: func(cmd *cobra.Command, args []string) {
-
+			cmd.Help()
 		},
 	}
 
 	cmd.AddCommand(newServerCmd())
 	cmd.AddCommand(newPetCommand())
 	cmd.AddCommand(newListPetsCommand())
+	cmd.AddCommand(loginCommand())
 
 	return &cmd
 }
